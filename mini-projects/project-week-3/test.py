78d41c622807f38887795325a64370a2baf75299
samx <sunildas2k19@gmail.com>
2022-11-15 22:30:59 +0000

    mini-projects

---
+++ mini-projects/project-week-3/test.py
@@ -0,0 +1,12 @@
+couriers = [{'courier_name': 'Sss'},
+{'courier_name': 'Ddd'}]
+
+      
+#for d in couriers:
+ #   d.update((k, "courier_name") for k, v in d.items() if v == "Sss")
+
+
+D = {'courier_name' : 'samhax'}
+D.update()
+couriers.update('courier_name', 'sam')
+print(couriers)
